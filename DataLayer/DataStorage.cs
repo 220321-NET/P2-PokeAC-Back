fc6f66851b5af00edd736d9d21366afd06e29b6f
Leo Doan <doanleo@yahoo.com>
2022-04-22 15:47:27 -0400

    hopium

--- DataLayer/DataStorage.cs
+++ DataLayer/DataStorage.cs
@@ -1,9 +1,30 @@
-﻿namespace DataLayer;
-//using Microsoft.Data.SqlClient;
+﻿using System.Linq;
+
+namespace DataLayer;
 public class DataStorage : IRepo
-{   
+{
+    private readonly PACDBContext _context;
+    public DataStorage(PACDBContext context)
+    {
+        _context = context;
+    }
     public User getUser(User user)
     {
-        return new User();
+    
     }
+
+    /*
+        me taking notes
+        this is how you add a user
+        pub user add(user)
+        _context.User.Add(user);
+        _context.SaveChanges();
+        return user;
+    
+        pub List<user> getalluser
+        return _context.Users.Select(i => i).ToListAsync();
+
+        
+
+    */
 }
