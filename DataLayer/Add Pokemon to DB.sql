8106dd34973d5141de432de00dd788a167c48ddc
Leo Doan <doanleo@yahoo.com>
2022-05-01 23:35:58 -0400

    sql

---
+++ DataLayer/Add Pokemon to DB.sql
@@ -0,0 +1,30 @@
+-- Select * from Pokedex;
+-- select * from Pokemon;
+-- select * from USERs;
+
+-- insert into pokemon values (4, 'Charmander', 5, 1, 0, 'fire')
+-- insert into pokemon values (7, 'Squirtle', 5, 1, 0, 'water')
+-- insert into pokemon values (1, 'Bulbasaur', 5, 1, 0, 'grass')
+-- insert into pokemon values (25, 'Pikachu', 5, 1, 0, 'electric')
+-- insert into pokemon values (133, 'Eevee', 5, 1, 0, 'normal')
+
+-- insert into pokemon values (92, 'Gastly', 5, 1, 0, 'ghost')
+-- insert into pokemon values (54, 'Psyduck', 5, 1, 0, 'water')
+-- insert into pokemon values (303, 'Mawile', 5, 1, 0, 'steel')
+-- insert into pokemon values (120, 'Staryu', 5, 1, 0, 'water')
+-- insert into pokemon values (63, 'Abra', 5, 1, 0, 'psychic')
+
+-- insert into pokemon values (198, 'Murkrow', 5, 1, 0, 'dark')
+-- insert into pokemon values (16, 'Pidgey', 5, 1, 0, 'flying')
+-- insert into pokemon values (50, 'Diglet', 5, 1, 0, 'ground')
+-- insert into pokemon values (95, 'Onix', 5, 1, 0, 'rock')
+-- insert into pokemon values (23, 'Ekans', 5, 1, 0, 'poison')
+
+-- insert into pokemon values (21, 'Spearow', 5, 1, 0, 'flying')
+-- insert into pokemon values (69, 'Bellsprout', 5, 1, 0, 'grass')
+-- insert into pokemon values (147, 'Dratini', 5, 1, 0, 'dragon')
+-- insert into pokemon values (77, 'Ponyta', 5, 1, 0, 'fire')
+-- insert into pokemon values (127, 'Pinser', 5, 1, 0, 'bug')
+
+select * from pokemon order by type1;
+
